2a52ea52164fdccc7f1f3577ccb8206a15cd40af
Chad Hietala <chadhietala@gmail.com>
2015-10-27 16:17:23 -0700

    fix unregister

--- tests/test.js
+++ tests/test.js
@@ -26,6 +26,33 @@ describe('raf-measure', () => {
     expect(measurer._queues.scroll.length).to.eql(1);
   });
 
+  it('should unregister events', () => {
+    let mScroll = measurer.register('scroll', {
+      callback: () => {}
+    });
+
+    let mResize = measurer.register('resize', {
+      callback: () => {}
+    });
+
+    let minViewPort = measurer.register('inViewPort', {
+      callback: () => {},
+      element: document.querySelector('div')
+    });
+
+    expect(measurer._queues.scroll.length).to.eql(1);
+    expect(measurer._queues.resize.length).to.eql(1);
+    expect(measurer._queues.scroll.length).to.eql(1);
+
+    measurer.unregister('scroll', mScroll);
+    measurer.unregister('resize', mResize);
+    measurer.unregister('inViewPort', minViewPort);
+
+    expect(measurer._queues.scroll.length).to.eql(0);
+    expect(measurer._queues.resize.length).to.eql(0);
+    expect(measurer._queues.scroll.length).to.eql(0);
+  });
+
   it('should fire the scroll handler when scroll happens', (done) => {
     let called = false;
     let args;
