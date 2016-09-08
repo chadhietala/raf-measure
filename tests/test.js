b2e6e213af1f5801475c269d583232e0b98eeca6
Stefan Penner <stefan.penner@gmail.com>
2016-09-07 23:28:42 -0700

    only RAF when there exists pending work

--- tests/test.js
+++ tests/test.js
@@ -6,40 +6,50 @@ describe('raf-measure', () => {
   afterEach(() => {
     window.scrollTo(0, 0);
     measurer.unregisterAll();
+    expect(measurer._animationFrame).to.eql(undefined);
   });
 
   it('should place handlers in the queue', () => {
+    expect(measurer._animationFrame).to.eql(undefined);
     measurer.register('scroll', {
       callback: () => {}
     });
+    expect(measurer._animationFrame).to.not.eql(undefined);
 
     measurer.register('resize', {
       callback: () => {}
     });
+    expect(measurer._animationFrame).to.not.eql(undefined);
 
     measurer.register('inViewPort', {
       callback: () => {},
       element: document.querySelector('div')
     });
+    expect(measurer._animationFrame).to.not.eql(undefined);
     expect(measurer._queues.scroll.length).to.eql(1);
     expect(measurer._queues.resize.length).to.eql(1);
     expect(measurer._queues.inViewPort.length).to.eql(1);
   });
 
   it('should unregister events', (done) => {
+    expect(measurer._animationFrame).to.eql(undefined);
+
     let mScroll = measurer.register('scroll', {
       callback: () => {}
     });
 
+    expect(measurer._animationFrame).to.not.eql(undefined);
     let mResize = measurer.register('resize', {
       callback: () => {}
     });
 
+    expect(measurer._animationFrame).to.not.eql(undefined);
     let minViewPort = measurer.register('inViewPort', {
       callback: () => {},
       element: document.querySelector('div')
     });
 
+    expect(measurer._animationFrame).to.not.eql(undefined);
     expect(measurer._queues.scroll.length).to.eql(1);
     expect(measurer._queues.resize.length).to.eql(1);
     expect(measurer._queues.inViewPort.length).to.eql(1);
@@ -50,6 +60,7 @@ describe('raf-measure', () => {
 
     setTimeout(() => {
       try {
+        expect(measurer._animationFrame).to.not.eql(undefined);
         expect(measurer._queues.scroll.length).to.eql(0);
         expect(measurer._queues.resize.length).to.eql(0);
         expect(measurer._queues.inViewPort.length).to.eql(0);
@@ -65,12 +76,14 @@ describe('raf-measure', () => {
     let called = false;
     let args;
 
+    expect(measurer._animationFrame).to.eql(undefined);
     measurer.register('scroll', {
       callback: (position) => {
         args = position;
         called = true;
       }
     });
+    expect(measurer._animationFrame).to.not.eql(undefined);
 
     window.scrollTo(0, 100);
 
@@ -80,6 +93,7 @@ describe('raf-measure', () => {
         expect(args.direction).to.eql('down');
         expect(args.scrollTop).to.eql(100);
         expect(args.scrollLeft).to.eql(0);
+        expect(measurer._animationFrame).to.not.eql(undefined);
         done();
       } catch (e) {
         done(e);
@@ -90,6 +104,8 @@ describe('raf-measure', () => {
   it('should fire the inViewPort handler when element enters the viewport', (done) => {
     let called = false;
     let args;
+
+    expect(measurer._animationFrame).to.eql(undefined);
     measurer.register('inViewPort', {
       callback: (element, bounding) => {
         args = bounding;
@@ -98,17 +114,18 @@ describe('raf-measure', () => {
       element: document.getElementById('marker')
     });
 
+    expect(measurer._animationFrame).to.not.eql(undefined);
     window.scrollTo(0, 2000);
 
     setTimeout(() => {
       try {
         expect(called).to.eql(true);
         expect(args).to.be.an('object');
+        expect(measurer._animationFrame).to.not.eql(undefined);
         done();
       } catch (e) {
         done(e);
       }
-
     }, 25);
   });
 });
